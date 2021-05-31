6778e48627cb4dbc6d05730481d128511f1a0b52
zanynn <zanynngoose@gmail.com>
2021-06-01 00:08:36 +0700

    create splash screen with logo

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:online_shop/pages/home.dart';
 
@@ -15,7 +16,14 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: AnimatedSplashScreen(
+        splash:
+            '[n]https://t4.ftcdn.net/jpg/03/34/07/75/360_F_334077587_NsRtBwBniM0FkYxHcMIDpmjAidUhGtLU.jpg',
+        nextScreen: Home(),
+        backgroundColor: Color(0xFF07080A),
+        splashTransition: SplashTransition.scaleTransition,
+        duration: 2000,
+      ),
     );
   }
 }
