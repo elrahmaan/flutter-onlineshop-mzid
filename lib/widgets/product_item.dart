8d6799c6b2003cd5631b1c7a9017c50cda41388c
Abdul Rahman Saleh <abdulrahmansaleh152@gmail.com>
2021-06-04 23:25:24 +0700

    update widget for single product item

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -3,58 +3,75 @@ import 'package:intl/intl.dart';
 
 class ProductItem extends StatelessWidget {
   final String image;
-  final int price;
   final String name;
+  final int price;
 
-  ProductItem({this.image, this.name, this.price});
+  ProductItem(this.image, this.name, this.price);
 
   @override
   Widget build(BuildContext context) {
     // variabel untuk mengatur format angka untuk harga
     var formatNumber = NumberFormat("#,###");
-
-    return Card(
-      child: Container(
-        height: MediaQuery.of(context).size.height * 0.3,
-        width: MediaQuery.of(context).size.width * 0.2 * 2 + 30,
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              flex: 3,
-              child: Container(
-                padding: EdgeInsets.symmetric(vertical: 10),
-                child: Container(
-                  width: 160,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      fit: BoxFit.fill,
-                      image: NetworkImage(image),
-                    ),
-                  ),
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.grey.withOpacity(.5),
+                offset: Offset(3, 2),
+                blurRadius: 7)
+          ]),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: ClipRRect(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(15),
+                  topRight: Radius.circular(15),
+                ),
+                child: Image.network(
+                  image,
+                  width: double.infinity,
+                  height: 200,
+                )),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.only(left: 2, right: 2),
+              child: Text(
+                name,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 10.0),
+                child: Text(
+                  formatNumber.format(price).toString() + " IDR",
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                      fontWeight: FontWeight.normal),
                 ),
               ),
-            ),
-            Expanded(
-              child: Column(
-                children: [
-                  Text(
-                    formatNumber.format(price).toString() + " IDR",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 17,
-                        color: Color(0xFF1C1C1C)),
-                  ),
-                  Container(
-                    child: Text(
-                      name,
-                      style: TextStyle(fontSize: 15),
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ],
-        ),
+              IconButton(
+                  icon: Icon(Icons.remove_red_eye_outlined), onPressed: () {})
+            ],
+          ),
+        ],
       ),
     );
   }
