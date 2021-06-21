a08acdffb8f5dd16c2fa28c333ffb225517f6c0c
zanynn <zanynngoose@gmail.com>
2021-06-21 23:47:59 +0700

    into - add intro page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:online_shop/pages/home/home_screen.dart';
 import 'package:online_shop/pages/login_page.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:online_shop/pages/splash/splash_screen.dart';
+import 'package:online_shop/routes.dart';
+import 'package:online_shop/theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,19 +19,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: theme(),
       home: AnimatedSplashScreen(
         splash:
             '[n]https://t4.ftcdn.net/jpg/03/34/07/75/360_F_334077587_NsRtBwBniM0FkYxHcMIDpmjAidUhGtLU.jpg',
-        nextScreen: LoginPage(),
+        nextScreen: SplashScreen(),
         backgroundColor: Color(0xFF07080A),
         splashTransition: SplashTransition.scaleTransition,
         duration: 2700,
       ),
+      // home: SplashScreen(),
+      // We use routeName so that we dont need to remember the name
+      routes: routes,
     );
   }
 }
