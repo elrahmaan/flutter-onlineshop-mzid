d5a172c311368aee4cc949bf57e8016852ce8611
zanynn <zanynngoose@gmail.com>
2021-06-21 23:50:53 +0700

    profile - add profile page

---
+++ lib/pages/profile/profile_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:online_shop/widgets/coustom_bottom_nav_bar.dart';
+
+import '../../enums.dart';
+import 'components/body.dart';
+
+class ProfileScreen extends StatelessWidget {
+  static String routeName = "/profile";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Profile"),
+      ),
+      body: Body(),
+      bottomNavigationBar: CustomBottomNavBar(selectedMenu: MenuState.profile),
+    );
+  }
+}
